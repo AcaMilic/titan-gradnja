469a47734aa8210fbf9f08a5259b7a6adb39f913
Vasilije <vasilijetrifunovic42@gmail.com>
2021-02-11 12:18:23 +0100

    promena u css da se pokazuju slike na iphone uredjajima i promena u php skripti stavljen drugi email

--- form/sendemail.php
+++ form/sendemail.php
@@ -6,7 +6,7 @@ if (isset($_POST['name']) && isset($_POST['message']) && isset($_POST['email']))
 	$message = $_POST['message'];
 	$mailFrom = $_POST['email'];
 	
-	$mailTo = "aleksandarmilic1995@gmail.com";
+	$mailTo = "titangradnja@gmail.com";
 	$headers = "Email: ".$mailFrom.".\r\n" ."Message: ".$message.".\r\n";
 	$txt = "You have received an e-mail from ".$name.".\n\n";
 
